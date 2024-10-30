0fe2fdd505a838d083c31c11a60efcb472183591
Elias Vestermark <eliasvester96@gmail.com>
2024-10-30 13:36:33 +0100

    Add project files.

---
+++ Data/Context/DBContext.cs
@@ -0,0 +1,10 @@
+﻿using Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context;
+
+public class DBContext(DbContextOptions<DBContext> options) : IdentityDbContext<UserEntity>(options)
+{
+}
---
+++ Data/Factories/BloggingContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Factories;
+
+public class BloggingContextFactory : IDesignTimeDbContextFactory<DbContext>
+{
+    public DbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DbContext>();
+        optionsBuilder.UseSqlServer("Server=tcp:elias-rika-server.database.windows.net,1433;Initial Catalog=RikaUserDB;Persist Security Info=False;User ID=RikaAdmin;Password=Bytmig123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        return new DbContext(optionsBuilder.Options);
+    }
+}
