69fea9da6bdc0c6d5cb6d7832a09121fe4b0ac80
Marcus <Marcuswix@yahoo.se>
2024-11-04 15:14:21 +0100

    Factory Test

---
+++ Test/UnitTest/TestFactories.cs
@@ -0,0 +1,26 @@
+﻿using Data.Factories;
+using Data.Models;
+using Moq;
+
+namespace Test.UnitTest;
+
+public class TestFactories
+{
+
+    [Fact]
+    public void ShouldConvertASignUpModelToUserEntity_ThenReturnUserEntity()
+    {
+        //Arrange
+        var signUpModel = new SignUpModel { Email = "test@test.se", FirstName = "Test", LastName = "Testsson", Password = "Oklart" };
+
+
+        //Act
+        var result = UserFactory.Create(signUpModel);
+
+        //Assert
+        Assert.Equal(result.Email, signUpModel.Email);
+        Assert.Equal(result.FirstName, signUpModel.FirstName);
+        Assert.Equal(result.LastName, signUpModel.LastName);
+        Assert.Equal(result.PasswordHash, signUpModel.Password);
+    }
+}
