5a62a53325d2183f571442e9e31dca70508076aa
Marcus <Marcuswix@yahoo.se>
2024-11-06 09:17:33 +0100

    "Showed Elias tha changes"

--- Data/Repositories/UserRepository.cs
+++ Data/Repositories/UserRepository.cs
@@ -25,26 +25,23 @@ public class UserRepository(DBContext context, UserServices userServices, UserMa
                 if(!_userServices.CheckIfEmailExistsAsync(model.Email))
                 {
                     UserEntity userEntity = UserFactory.Create(model);
+                    //var result = await _context.Users.AddAsync(userEntity);
                     var userResult = await _userManager.CreateAsync(userEntity, model.Password);
 
                     if(userResult.Succeeded)
-                    {
-                        //var result = await _context.Users.AddAsync(userEntity);
+                    {                       
                         await _context.SaveChangesAsync();
                         return new StatusModel { StatusCode = 201, Message = "User is successfully created" };
                     }
                 }
-
                 return new StatusModel { StatusCode = 409, Message = "Email already exists" };
             }
-
             return new StatusModel { StatusCode = 400, Message = "" };
         }
         catch (Exception ex)
         {
             Debug.WriteLine("AddUserToDatabaseAsync", ex.Message);
         }
-
         return new StatusModel { StatusCode = 500, Message = "Internal server error" };
     }
 
