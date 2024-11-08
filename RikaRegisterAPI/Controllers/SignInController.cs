19e827298bd4e888d93e3edb155ba1a9e2883788
Marcus <Marcuswix@yahoo.se>
2024-11-08 12:20:22 +0100

    "Using normal contructors"

--- RikaRegisterAPI/Controllers/SignInController.cs
+++ RikaRegisterAPI/Controllers/SignInController.cs
@@ -4,40 +4,60 @@ using Data.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using System.Diagnostics;
 
 namespace RikaRegisterAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class SignInController(UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager, GenerateJwtTokenFactory tokenFactory) : ControllerBase
+    public class SignInControllerFactory : ControllerBase
     {
-        private readonly UserManager<UserEntity> _userManager = userManager;
-        private readonly SignInManager<UserEntity> _signInManager = signInManager;
-        private readonly GenerateJwtTokenFactory _tokenFactory = tokenFactory;
-
-        [HttpPost]
-        public async Task<IActionResult> SignInAsync([FromBody] SignInModel signInModel)
+        public class SignInController : ControllerBase
         {
-            if (ModelState.IsValid)
-            {
-                var result = await _signInManager.PasswordSignInAsync(signInModel.Email, signInModel.Password, signInModel.RememberMe, false);
-                if (result.Succeeded)
-                {
-                    var user = await _userManager.FindByEmailAsync(signInModel.Email);
-                    if (user != null)
-                    {
-                        var token = _tokenFactory.GenerateJwtToken(user);
-                        return Ok(new
-                        {
-                            jwttoken = token,
-                        });
-                    }
-                }
+            private readonly UserManager<UserEntity> _userManager;
+            private readonly SignInManager<UserEntity> _signInManager;
+            private readonly GenerateJwtTokenFactory _tokenFactory;
 
-                return Unauthorized();
+            public SignInController(UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager, GenerateJwtTokenFactory tokenFactory)
+            {
+                _userManager = userManager;
+                _signInManager = signInManager;
+                _tokenFactory = tokenFactory;
             }
 
-            return BadRequest();
+
+            [HttpPost]
+            public async Task<IActionResult> SignInAsync([FromBody] SignInModel signInModel)
+            {
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        var result = await _signInManager.PasswordSignInAsync(signInModel.Email, signInModel.Password, signInModel.RememberMe, false);
+                        if (result.Succeeded)
+                        {
+                            var user = await _userManager.FindByEmailAsync(signInModel.Email);
+                            if (user != null)
+                            {
+                                var token = _tokenFactory.GenerateJwtToken(user);
+                                return Ok(new
+                                {
+                                    jwttoken = token,
+                                });
+                            }
+                        }
+
+                        return Unauthorized("User not found");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+
+                return BadRequest("Invalid Data");
+            }
         }
     }
 }
